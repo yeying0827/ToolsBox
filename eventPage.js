01dd831fe8077d64137ebc2b7792adb4df0b1111
weaming <iweaming@gmail.com>
2016-03-22 18:25:14 +0800

    update

--- eventPage.js
+++ eventPage.js
@@ -1,6 +1,26 @@
+// Google searchEngine
+chrome.storage.local.get('searchEngine', function(items) {
+  if (!items.searchEngine){
+    chrome.storage.local.set({'searchEngine': "https://gg.ruanjiadeng.com"}, function(){console.log('Saved: '+value)});
+  }
+});
+
 chrome.omnibox.onInputEntered.addListener(omniboxFunc);
 function omniboxFunc(text){
     chrome.storage.local.get('searchEngine', function(items){
         window.open(items.searchEngine + "/search?q=" + text, "_blank");
     });
 }
+
+Right click menu
+var createProperties = {
+    "type": "normal",
+    "id": "007",
+    "title": "Base64编码当前图片",
+    "onclick": "function(){console.log('llll')}",
+    "contexts": ["image"]
+}
+chrome.contextMenus.create(createProperties, function(){
+    console.log('chrom ')
+    alert('sb');
+})
--- ext/singleJS/linkCursor.js
+++ ext/singleJS/linkCursor.js
@@ -1,6 +1,5 @@
 links = document.getElementsByTagName('a');
 var imgURL = chrome.extension.getURL("static/img/handCursor.png");
-console.log(imgURL);
 for (i=0;i<links.length;i++){
 	if (links[i].target === "_blank"){
 		links[i].style.cursor = 'url("' + imgURL + '"), pointer';
